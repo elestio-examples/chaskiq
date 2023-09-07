b81889164b9c3eddee06750615aac095028f2087
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-07 16:43:22 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -10,11 +10,3 @@ docker-compose exec -T server bash -c "rails db:schema:load"
 docker-compose exec -T server bash -c "rails db:seed"
 docker-compose exec -T server bash -c "rake admin_generator"
 
-
-apt install -y redis-tools
-
-#register the local server in the web ui
-redis_insight_target=$(docker-compose port redisinsight 8001)
-curl --output /dev/null --header "Content-Type: application/json" \
- --request POST --data '{ "name": "local", "connectionType": "STANDALONE", "host": "redis","port": 6379,"password": "'"$SOFTWARE_PASSWORD"'"}' \
- http://$redis_insight_target/api/instance/
