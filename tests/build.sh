9332992392289c03c1f8f220781a2ad1947fa5db
Prem chavhan <pchavhan@logiquad.com>
2023-11-07 19:25:36 +0530

    fixes

--- tests/build.sh
+++ tests/build.sh
@@ -6,3 +6,35 @@ sed -i "s~ARG NODE_MAJOR~ARG NODE_MAJOR=16~g" Dockerfile
 sed -i "s~ARG YARN_VERSION~ARG YARN_VERSION=1.13.0~g" Dockerfile
 sed -i "s~ARG BUNDLER_VERSION~ARG BUNDLER_VERSION=2.3.26~g" Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/chaskiq:latest | docker load
+
+charset="A-Za-z0-9"
+SECRET_KEY_BASE=$(cat /dev/urandom | tr -dc "$charset" | head -c 128)
+
+cat << EOT >> ./.env
+
+SECRET_KEY_BASE=${SECRET_KEY_BASE}
+SMTP_ADDRESS=tuesday.mxrouting.net
+DEFAULT_SENDER_EMAIL=${SMTP_LOGIN}
+SMTP_USERNAME=${SMTP_LOGIN}
+SMTP_PASSWORD=${SMTP_PASSWORD}
+
+EOT
+
+cat <<EOT > ./servers.json
+{
+    "Servers": {
+        "1": {
+            "Name": "local",
+            "Group": "Servers",
+            "Host": "172.17.0.1",
+            "Port": 5822,
+            "MaintenanceDB": "postgres",
+            "SSLMode": "prefer",
+            "Username": "postgres",
+            "PassFile": "/pgpass"
+        }
+    }
+}
+EOT
+
+rm post.txt
--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 docker-compose up -d;
-sleep 300s;
+sleep 60s;
+
+docker-compose exec -T server bash -c "rails db:create"
+docker-compose exec -T server bash -c "rails db:schema:load"
+docker-compose exec -T server bash -c "rails db:seed"
+docker-compose exec -T server bash -c "rake admin_generator"
